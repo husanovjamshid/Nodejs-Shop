5ffee597396e3e407995dc9e1470e8dcf0b7c9f5
husanovjamshid <husanovjamshid11@gmail.com>
2023-04-30 16:28:35 +0500

    add routes

--- src/server.js
+++ src/server.js
@@ -10,27 +10,24 @@ function httpServer(req, res) {
 
 	app.get('/categories', (req, res) => {
 		res.setHeader('Content-Type', 'application/json');
-		res.end(JSON.stringify(readFile('category')));
+		const category = readFile('category');
+		const subCategory = readFile('subCategory');
+
+		category.map((item) => {
+			item.subCategory = subCategory.find((items) => {
+				return items.category_id == items.category_id, delete items.category_id;
+			});
+		});
+
+		res.end(JSON.stringify(category));
 	});
 
-    app.get('/subcategories', (req, res) => {
+	app.get('/subcategories', (req, res) => {
 		res.setHeader('Content-Type', 'application/json');
 		res.end(JSON.stringify(readFile('subCategory')));
 	});
-
-	// res.end(() => {
-	// 	const data = readFile('users');
-	// 	const news = {
-	// 		user_id: 1,
-	// 		username: 'Jamshid',
-	// 		password: '12Aa$1oedijaoisidjao92831jsa$as',
-	// 	};
-
-	//     data.push(news)
-	// 	writeFile('users', data)
-	// });
 }
 
 const server = http.createServer(httpServer);
 
-server.listen(PORT, () => console.log('Server Running on port ' + PORT));
+server.listen(PORT, () => console.log('Server running on port ' + PORT));
